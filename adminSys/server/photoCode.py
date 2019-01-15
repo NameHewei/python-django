21c0731beea0d1ef887911d6a5c86059c45c03a6
NameHewei <heweimailbox@foxmail.com>
2019-01-15 22:04:42 +0800

    regular update

--- adminSys/server/photoCode.py
+++ adminSys/server/photoCode.py
@@ -4,25 +4,34 @@ import random
 
 # 随机大写字母
 def upperChar():
+    # chr() 方法返回数字对应的英文字母
     return chr(random.randint(65, 90))
+
+# 随机颜色
+def randomColor():
+    return (random.randint(50,255), random.randint(50,255), random.randint(50,255))
 
 width = 360
 height = 50
 
 image = Image.new('RGB', (width, height), (255, 255, 255))
-# use a truetype font 创建Font对象:
+
+# 用 truetype font 创建Font对象:  
 font = ImageFont.truetype('arial.ttf', 40)
+
 # 创建Draw对象:
 draw = ImageDraw.Draw(image)
+
 # 填充每个像素:
 for x in range(width):
     for y in range(height):
-        draw.point((x, y), fill=(222, 124, 224))
+        draw.point((x, y), fill=(255, 255,255))
+        
 # 输出文字:
 for t in range(4):
     a = (40 * (2*t + 1) , 5)
-    print(a, t)
-    draw.text(a, upperChar(), font=font, fill=(0,0,0))
+    draw.text(a, upperChar(), font=font, fill=randomColor())
+
 # 模糊:
-image = image.filter(ImageFilter.BLUR)
+# image = image.filter(ImageFilter.BLUR)
 image.save('code.jpg', 'jpeg')
